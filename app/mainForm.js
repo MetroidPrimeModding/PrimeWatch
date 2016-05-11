c9fd9801850c517c64fa8bc6d29ec55ee678c185
Pwootage <pwootage@gmail.com>
2016-05-10 23:28:01 -0600

    Organize the repo a bit

--- app/mainForm.js
+++ app/mainForm.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const electron = require('electron');
 const app = electron.app;
@@ -6,21 +6,17 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
 
-app.on('window-all-closed', function()
-{
-  if(process.platform != 'darwin')
-  {
+app.on('window-all-closed', () => {
     app.quit();
-  }
 });
 
-app.on('ready', function()
-{
+app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 600, height: 800, frame: false, minWidth: 600, minHeight: 800, title: "Metroid Prime Randomizer"});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', function()
-  {
+  mainWindow.loadURL('file://' + __dirname + '/../web/html/index.html');
+  mainWindow.webContents.openDevTools({
+    mode: "undocked"
+  });
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
