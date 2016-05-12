ed2aaec18a082766e53d2cdefbff1a49faf74f29
izzyaxel <bencosgriff@gmail.com>
2016-05-12 15:33:32 -0400

    Help modals and refinements

--- web/js/index.js
+++ web/js/index.js
@@ -16,6 +16,18 @@ function inputBlur(i)
   }
 }
 
+function toggle(chbox)
+{
+  if(chbox.checked == true)
+  {
+    chbox.checked = false;
+  }
+  else
+  {
+    chbox.checked = true;
+  }
+}
+
 (function()
 {
   var remote = require('remote');
@@ -23,6 +35,16 @@ function inputBlur(i)
 
   function init()
   {
+    document.getElementById("darken_div").addEventListener("click", function(e)
+    {
+      darken_div.style.display = 'none';
+      fakeClose_button.style.display = 'none';
+      warpHelp_span.style.display= 'none';
+      ilsHelp_span.style.display = 'none';
+      popupHelp_span.style.display = 'none';
+      musicHelp_span.style.display = 'none';
+    });
+
     document.getElementById("min_button").addEventListener("click", function(e)
     {
       var window = BrowserWindow.getFocusedWindow();
@@ -34,6 +56,36 @@ function inputBlur(i)
       var window = BrowserWindow.getFocusedWindow();
       window.close();
     });
+
+    document.getElementById("warp_span").addEventListener("click", function(e)
+    {
+      var warp = document.getElementById("warp_check");
+      warp.checked = !warp.checked;
+      //darken_div.style.display = 'inline-block';
+      //warpHelp_span.style.display = 'inline-block';
+      //fakeClose_button.style.display = 'inline-block';
+    });
+
+    document.getElementById("ils_span").addEventListener("click", function(e)
+    {
+      darken_div.style.display = 'inline-block';
+      ilsHelp_span.style.display = 'inline-block';
+      fakeClose_button.style.display = 'inline-block';
+    });
+
+    document.getElementById("popup_span").addEventListener("click", function(e)
+    {
+      darken_div.style.display = 'inline-block';
+      popupHelp_span.style.display = 'inline-block';
+      fakeClose_button.style.display = 'inline-block';
+    });
+
+    document.getElementById("music_span").addEventListener("click", function(e)
+    {
+      darken_div.style.display = 'inline-block';
+      musicHelp_span.style.display = 'inline-block';
+      fakeClose_button.style.display = 'inline-block';
+    });
   };
 
   document.onreadystatechange = function()
@@ -41,6 +93,18 @@ function inputBlur(i)
     if(document.readyState = "complete")
     {
       init();
+      darken_div.style.display = 'none';
+      fakeClose_button.style.display = 'none';
+      warpHelp_span.style.display = 'none';
+      ilsHelp_span.style.display = 'none';
+      popupHelp_span.style.display = 'none';
+      musicHelp_span.style.display = 'none';
     }
   };
 })();
+
+function popupWarpHelp(link, windowName)
+{
+  if(!window.focus) return true;
+  window.open(link, windowName, 'width=400,height=130,scrollbars=no');
+}
