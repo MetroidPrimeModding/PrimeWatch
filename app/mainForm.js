e95fea3789eba001231ec49c94c73abb0617639f
izzyaxel <bencosgriff@gmail.com>
2016-05-14 19:52:08 -0400

    Game-specific settings, game toggle

--- app/mainForm.js
+++ app/mainForm.js
@@ -6,11 +6,10 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
 
-app.on('window-all-closed', () => {
-    app.quit();
-});
+app.on('window-all-closed', () => {app.quit();});
 
-app.on('ready', () => {
+app.on('ready', () =>
+{
   mainWindow = new BrowserWindow({width: 600, height: 800, frame: false, minWidth: 600, minHeight: 800, title: "Metroid Prime Randomizer"});
   mainWindow.loadURL('file://' + __dirname + '/../web/html/index.html');
   //mainWindow.webContents.openDevTools({mode: "undocked"});
--- web/js/index.js
+++ web/js/index.js
@@ -16,6 +16,84 @@ function inputBlur(i)
   }
 }
 
+function prime1OnMouseUp()
+{
+  document.getElementById("gameSelectPrime1_div").style.backgroundColor = "#1e1e1d";
+  document.getElementById("gameSelectPrime2_div").style.backgroundColor = "#0e0e0d";
+
+  document.getElementById("primeSeed_in").style.visibility = "visible";
+  document.getElementById("primeOptions_div").style.visibility = "visible";
+  document.getElementById("primeOptionsHeader_div").style.visibility = "visible";
+  document.getElementById("primeOptionsSectionTitle_span").style.visibility = "visible";
+  document.getElementById("primeOptionsHeaderDivider_div").style.visibility = "visible";
+  document.getElementById("primeFrigate_span").style.visibility = "visible";
+  document.getElementById("primeFrigate_check").style.visibility = "visible";
+  document.getElementById("primeWarp_span").style.visibility = "visible";
+  document.getElementById("primeWarp_check").style.visibility = "visible";
+  document.getElementById("primeIls_span").style.visibility = "visible";
+  document.getElementById("primeIls_check").style.visibility = "visible";
+  document.getElementById("primePopup_span").style.visibility = "visible";
+  document.getElementById("primePopup_check").style.visibility = "visible";
+  document.getElementById("primeMusic_span").style.visibility = "visible";
+  document.getElementById("primeMusic_check").style.visibility = "visible";
+
+  document.getElementById("prime2Seed_in").style.visibility = "hidden";
+  document.getElementById("prime2Options_div").style.visibility = "hidden";
+  document.getElementById("prime2OptionsHeader_div").style.visibility = "hidden";
+  document.getElementById("prime2OptionsSectionTitle_span").style.visibility = "hidden";
+  document.getElementById("prime2OptionsHeaderDivider_div").style.visibility = "hidden";
+  document.getElementById("prime2Physics_span").style.visibility = "hidden";
+  document.getElementById("prime2Physics_check").style.visibility = "hidden";
+  document.getElementById("prime2Warp_span").style.visibility = "hidden";
+  document.getElementById("prime2Warp_check").style.visibility = "hidden";
+  document.getElementById("prime2Ils_span").style.visibility = "hidden";
+  document.getElementById("prime2Ils_check").style.visibility = "hidden";
+  document.getElementById("prime2Popup_span").style.visibility = "hidden";
+  document.getElementById("prime2Popup_check").style.visibility = "hidden";
+  document.getElementById("prime2Music_span").style.visibility = "hidden";
+  document.getElementById("prime2Music_check").style.visibility = "hidden";
+  console.log("prime 1");
+}
+
+function prime2OnMouseUp()
+{
+  document.getElementById("gameSelectPrime2_div").style.backgroundColor = "#1e1e1d";
+  document.getElementById("gameSelectPrime1_div").style.backgroundColor = "#0e0e0d";
+
+  document.getElementById("prime2Seed_in").style.visibility = "visible";
+  document.getElementById("prime2Options_div").style.visibility = "visible";
+  document.getElementById("prime2OptionsHeader_div").style.visibility = "visible";
+  document.getElementById("prime2OptionsSectionTitle_span").style.visibility = "visible";
+  document.getElementById("prime2OptionsHeaderDivider_div").style.visibility = "visible";
+  document.getElementById("prime2Physics_span").style.visibility = "visible";
+  document.getElementById("prime2Physics_check").style.visibility = "visible";
+  document.getElementById("prime2Warp_span").style.visibility = "visible";
+  document.getElementById("prime2Warp_check").style.visibility = "visible";
+  document.getElementById("prime2Ils_span").style.visibility = "visible";
+  document.getElementById("prime2Ils_check").style.visibility = "visible";
+  document.getElementById("prime2Popup_span").style.visibility = "visible";
+  document.getElementById("prime2Popup_check").style.visibility = "visible";
+  document.getElementById("prime2Music_span").style.visibility = "visible";
+  document.getElementById("prime2Music_check").style.visibility = "visible";
+
+  document.getElementById("primeSeed_in").style.visibility = "hidden";
+  document.getElementById("primeOptions_div").style.visibility = "hidden";
+  document.getElementById("primeOptionsHeader_div").style.visibility = "hidden";
+  document.getElementById("primeOptionsSectionTitle_span").style.visibility = "hidden";
+  document.getElementById("primeOptionsHeaderDivider_div").style.visibility = "hidden";
+  document.getElementById("primeFrigate_span").style.visibility = "hidden";
+  document.getElementById("primeFrigate_check").style.visibility = "hidden";
+  document.getElementById("primeWarp_span").style.visibility = "hidden";
+  document.getElementById("primeWarp_check").style.visibility = "hidden";
+  document.getElementById("primeIls_span").style.visibility = "hidden";
+  document.getElementById("primeIls_check").style.visibility = "hidden";
+  document.getElementById("primePopup_span").style.visibility = "hidden";
+  document.getElementById("primePopup_check").style.visibility = "hidden";
+  document.getElementById("primeMusic_span").style.visibility = "hidden";
+  document.getElementById("primeMusic_check").style.visibility = "hidden";
+  console.log("prime 2");
+}
+
 (function()
 {
   var remote = require('remote');
@@ -33,6 +111,18 @@ function inputBlur(i)
     {
       var window = BrowserWindow.getFocusedWindow();
       window.close();
+    });
+
+    document.addEventListener('drop', function(e)
+    {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+
+    document.addEventListener('dragover', function(e)
+    {
+      e.preventDefault();
+      e.stopPropagation();
     });
   };
 
